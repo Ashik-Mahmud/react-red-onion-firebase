6a209b431d5346b4706af717e760f44a6a4b0cc9
ashikmahmud <ashikmamud187@gmail.com>
2022-04-15 19:00:00 +0600

    done hero section area

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -6,11 +6,20 @@ const Hero = () => {
       <div className="container">
         <div className="hero-content">
           <h1>Best Food Waiting for you belly come!!</h1>
-          <form action="">
-            <div className="input-group">
+          <form action="" className="search-wrapper">
+            <div className="input-group-custom">
               <input type="search" placeholder="search" id="search" />
-              <button>Search</button>
+              <button className="btn-app">Search</button>
             </div>
+            <ul className="search-terms d-flex justify-content-center g-3">
+              <li>
+                <small>People Search -</small>
+              </li>
+              <li className="mx-1 cursor-pointer badge bg-danger">Dish</li>
+              <li className="mx-1 cursor-pointer badge bg-danger">Lunch</li>
+              <li className="mx-1 cursor-pointer badge bg-danger">Dinner</li>
+              <li className="mx-1 cursor-pointer badge bg-danger">Breakfast</li>
+            </ul>
           </form>
         </div>
       </div>
