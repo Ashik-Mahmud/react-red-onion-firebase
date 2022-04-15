e20148cf6a421c18ca02c07a09130ac1d417a1f1
ashikmahmud <ashikmamud187@gmail.com>
2022-04-15 17:24:07 +0600

    initialization red-onion project using react and firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import SignIn from './Pages/Login/SignIn/SignIn';
+import SignUp from './Pages/Login/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Routes>
+         <Route path='/' element={<Home />} />
+         <Route path='/home' element={<Home />} />
+         <Route path='/login' element={<SignIn />} />
+         <Route path='/sign-up' element={<SignUp />} />
+     </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/FoodDetails/FoodDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FoodDetails = () => {
+  return (
+    <section id="food-details">
+      <div className="container">
+        <h1>FoodDetails</h1>
+      </div>
+    </section>
+  );
+};
+
+export default FoodDetails;
