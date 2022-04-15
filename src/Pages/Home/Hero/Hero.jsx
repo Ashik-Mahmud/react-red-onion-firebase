266d787e11c5b48415bfeb0587f7ef0a7ff2f91c
ashikmahmud <ashikmamud187@gmail.com>
2022-04-15 18:00:55 +0600

    yeah done almost design of hero section:

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import "./Hero.css";
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="container"></div>
+    <div className="hero text-center">
+      <div className="container">
+        <div className="hero-content">
+          <h1>Best Food Waiting for you belly come!!</h1>
+          <form action="">
+            <div className="input-group">
+              <input type="search" placeholder="search" id="search" />
+              <button>Search</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 import logo from "../../Assets/images/logo2.png";
 const Navbar = () => {
   return (
     <header id="navbar" className="shadow-sm py-2">
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container">
-          <a href="/" className="logo navbar-brand">
+          <NavLink to="/" className="logo navbar-brand">
             <img src={logo} alt="app-logo" width={150} />
-          </a>
+          </NavLink>
           <ul className="navbar-nav">
             <li className="nav-item px-2">
-              <a className="nav-link" href="/">
+              <NavLink className="nav-link" to="/">
                 <MdOutlineShoppingCart />{" "}
                 <sup className="badge bg-danger rounded-circle">0</sup>
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item px-2">
-              <a className="nav-link" href="/">
+              <NavLink className="nav-link" to="/login">
                 Login
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item px-2">
-              <a className="btn-app" href="/">
+              <NavLink className="btn-app" to="/sign-up">
                 Sign Up
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
