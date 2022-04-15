6bbbc83608d389af8cb965aa5b16e1dc95ba17b5
ashikmahmud <ashikmamud187@gmail.com>
2022-04-16 03:15:33 +0600

    initialization firebase

--- src/Pages/Login/SocialLogin/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin/SocialLogin.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { AiOutlineGithub, AiOutlineGoogle } from "react-icons/ai";
+import { auth } from "../../../Firebase/Firebase";
 import "../Styles/Styles.css";
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  if (user) {
+    console.log(user);
+  }
   return (
     <div id="social-login">
       <p className="line">Or</p>
       <div className="btn-group text-center d-flex gap-2 justify-content-center">
-        <button className="btn btn-danger">
+        <button className="btn btn-danger" onClick={() => signInWithGoogle()}>
           <AiOutlineGoogle /> Google
         </button>
         <button className="btn btn-danger">
